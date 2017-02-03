533ff99161d1e3e7c151bddf36e48e1253f150ae
Ethan Coyne <ethancoyne@Ethans-MacBook-Pro.local>
2017-02-03 15:01:28 -0600

    completed walkthrough. search functionality complete

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,9 +3,24 @@ var app = angular.module('myApp', []);
 app.controller('GifController', function (GifGetter) {
   console.log('connected?');
 
-GifGetter.randomGif();
+var vm = this;
 
 
+
+GifGetter.randomGif().then(function(res) {
+  console.log('from the controller: ', res);
+  vm.randomGifUrl = res.data.image_url;
+});
+
+// console.log('test:', GifGetter.randomGif());
+
+
+//searching for gifs
+vm.searchGif = function(searchQuery) {
+GifGetter.searchGif(searchQuery).then(function(res) {
+  vm.searchGifUrl = res.data;
+});
+}
 
 
 
--- public/scripts/giphyService.js
+++ public/scripts/giphyService.js
@@ -1,17 +1,34 @@
 app.service('GifGetter', function($http) {
   var publicAPIkey = 'dc6zaTOxFJmzC';
   var giphyUrl = 'http://api.giphy.com/v1/gifs/';
+
+  //  get random gif function:
   this.randomGif = function () {
-    $http({
+    return $http({
       type: "GET",
       url: giphyUrl + 'random?api_key=' + publicAPIkey
     }).then(function(response) {
-      console.log(response);
+      // console.log(response);
+      return response.data;
     }).catch(function(error) {
       console.log('error: ', error);
     });
 
   }
 
+
+  //searched gif function
+  this.searchGif = function (query) {
+    return $http({
+      type: "GET",
+      url: giphyUrl + 'search?q=' + query + '&api_key=' + publicAPIkey
+    }).then(function(response) {
+      // console.log(response);
+      return response.data;
+    }).catch(function(error) {
+      console.log('error: ', error);
+    });
+
+  }
 
 });// end app.service
